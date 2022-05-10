46d054458bb718df74e02a8b88e889afa1c7c6da
ShivenJain <87224661+ShivenJain@users.noreply.github.com>
2022-05-10 14:46:31 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,30 @@
+canvas=document.getElementById("mycanvas");
+ctx=canvas.getContext("2d");
+color="red";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown",mymousedown);
+function mymousedown(e)
+{
+    color=document.getElementById("color").value;
+    console.log(color);
+    mousex=e.clientX-canvas.offsetLeft;
+    mousey=e.clientY-canvas.offsetTop;
+    console.log("X = " + mousex + " ,Y = " + mousey);
+    circle(mousex,mousey);
+}
+function circle(mousex,mousey)
+{
+    ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(mousex,mousey,40,0,2*Math.PI);
+ctx.stroke();
+}
+function cleararea()
+{
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
